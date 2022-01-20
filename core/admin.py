15a055edb18fc9fe64185069a8fd13f441caaca7
Rafael Teixeira <rafaeldtx@hotmail.com>
2022-01-20 08:27:08 -0300

    Criação das tabelas

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from core.models import Lista
+from core.models import Categoria
 
 @admin.register(Lista)
 class ListaAdmin(admin.ModelAdmin):
     list_display=('id','produto','preco','tipo','quantidade','descricao','mercado','usuario')
     ordering=('tipo','produto',)
+    
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display=('tipo',)
+    ordering=('tipo',)
+    
--- core/models.py
+++ core/models.py
@@ -1,16 +1,28 @@
+from multiprocessing import Value
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 
-class Lista(models.Model):
 
+
+class Categoria(models.Model):
+    tipo=models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.tipo
+
+class Lista(models.Model):
     produto=models.CharField(max_length=80)
     quantidade=models.IntegerField()
-    preco=models.DecimalField(max_digits=8,decimal_places=2)
-    tipo=models.CharField(max_length=50)
+    preco=models.DecimalField(max_digits=8,decimal_places=2,verbose_name="Preco por unidade")
+    tipo=models.ForeignKey(Categoria,on_delete=models.CASCADE)
     descricao=models.TextField(null=True, blank=True)
     mercado=models.CharField(max_length=80)
     usuario=models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return self.produto
+         return self.produto
+
+     
+
+
